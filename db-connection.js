5373c6856ae0f875a9979d2200a77b5ca45bd028
2104571A <109129671+2104571A@users.noreply.github.com>
2022-07-12 15:13:27 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,15 +1,15 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'database-1.codmpreskocs.us-east-1.rds.amazonaws.com',
-    port: '3306',
-    user: 'admin',
-    password: '12345678',
-    database: 'movie_info'
-});
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: 'database-1.codmpreskocs.us-east-1.rds.amazonaws.com',
+//     port: '3306',
+//     user: 'admin',
+//     password: '12345678',
+//     database: 'movie_info'
+// });
 
-connection.connect(err => {  // test out connetion and console.log error if there is one
-    if (err) throw err;
-    console.log('Connected To DB');
-}); 
-module.exports = connection;
+// connection.connect(err => {  // test out connetion and console.log error if there is one
+//     if (err) throw err;
+//     console.log('Connected To DB');
+// }); 
+// module.exports = connection;
 
